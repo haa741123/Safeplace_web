659e4fd789a51fb34307a5005cf8f5fdd229068d
wjswlgns03 <jjh030726@tukorea.ac.kr>
2024-06-25 23:31:26 +0900

    "수정"

---
+++ static/js/hamin.js
@@ -0,0 +1,6 @@
+// hamin.js
+
+const h_config = {
+    apikey: "H9Fx7JetuR86sqMxS20M21GvcMVkaS2T1wbDQiA0"
+};
+
---
+++ static/js/jihoon.js
@@ -0,0 +1,6 @@
+// jihoon.js
+
+const j_config = {
+    apikey: "dk2zYLr0F0a2sZiMeZo8c4dNNUBSOH177Iu95CNd"
+};
+
